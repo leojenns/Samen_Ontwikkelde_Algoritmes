71d24826238a305cf011e0b27ae89ecfdb1d3983
leojenns <leo.jenns.s2@gmail.com>
2017-04-05 16:09:09 +0200

    add .pdf in correct repo

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -6,7 +6,9 @@ IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &imag
 	for (int collum = 0 ;collum  <= height-1 ; collum++) {
 		for (int row = 0 ; row <= width-1 ; row++) {
 			RGB color = image.getPixel(row, collum);
-			Intensity get = color.b ;
+			Intensity get = color.b ;// single color
+		  //Intensity get  =  (color.r + color.g + color.b) /3 // average
+		  //Intensity get  =  color.r*0.21  + color.g*0.72 + color.b*0.07 // luminosity
 			grey->setPixel(row, collum, get);
 		}
 	}
